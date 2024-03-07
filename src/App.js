aaebd825545b61e0c492c44204fe7dbd7fdcd165
iamaices <premchit.ko@gmail.com>
2024-03-07 13:52:24 +0700

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 import PokemonList from './components/PokemonList';
 
 const App = () => {
+  const [showPokemonList, setShowPokemonList] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowPokemonList(true);
+  };
+
   return (
-    <div>
-      <PokemonList />
+    <div className="App">
+      <header className="App-header">
+        <h1>Pokemon API</h1>
+        <button className="button" onClick={handleButtonClick}>Get Pokemon Dex</button>
+        {showPokemonList && <PokemonList />}
+      </header>
     </div>
   );
 };
